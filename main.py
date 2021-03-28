ab6d656f616403e0b5991f49b4f044154a111c83
bashleig <din.latypov@gmail.com>
2021-03-28 18:02:05 +0300

    bow + f1 is done

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import spacy
 from dask import delayed
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.model_selection import train_test_split, cross_validate, GridSearchCV, RandomizedSearchCV
+
 
 # from spacy.lang.en.stop_words import STOP_WORDS
 
@@ -49,17 +52,20 @@ def split(data: pd.DataFrame, conf: dict):
 @delayed
 def spacy_lemmatization(data: pd.DataFrame):
     name = inspect.stack()[0][3]  # function name
+    print('{} ...'.format(name))
+
     try:
         data = pd.read_csv('data/03_primary/{}_train.csv'.format(name))
+        print("Loaded preprocessed from directory")
         return data
     except FileNotFoundError:
         # preprocessed = preprocess(data, conf)
         nlp = spacy.load('en_core_web_sm')
         # lemmatized = data.text.apply(lemmatization)
-        print('{} ...'.format(name))
         data.text = data.text.apply(lambda sent:
                                     ' '.join([token.lemma_ for token in nlp(sent)]))
         train, val, test = split(data, conf)
+        data.label = data.label.apply(lambda label: 1 if label == 'Relevant' else 0)
         train.to_csv('data/03_primary/{}_train.csv'.format(name))
         val.to_csv('data/03_primary/{}_val.csv'.format(name))
         test.to_csv('data/03_primary/{}_test.csv'.format(name))
@@ -68,17 +74,20 @@ def spacy_lemmatization(data: pd.DataFrame):
 @delayed
 def spacy_lemmatization_rm_stopwords(data: pd.DataFrame):
     name = inspect.stack()[0][3]  # function name
+    print('{} ...'.format(name))
     try:
         data = pd.read_csv('data/03_primary/{}_train.csv'.format(name))
+        print("Loaded preprocessed from directory")
         return data
     except FileNotFoundError:
         # preprocessed = preprocess(data, conf)
         nlp = spacy.load('en_core_web_sm')
         # lemmatized = data.text.apply(lemmatization)
-        print('{} ...'.format(name))
+
         data.text = data.text.apply(lambda sent:
                                     ' '.join([token.lemma_ for token in nlp(sent)
                                               if nlp.vocab[token.text].is_stop is False]))
+        data.label = data.label.apply(lambda label: 1 if label == 'Relevant' else 0)
         train, val, test = split(data, conf)
         train.to_csv('data/03_primary/{}_train.csv'.format(name))
         val.to_csv('data/03_primary/{}_val.csv'.format(name))
@@ -95,32 +104,67 @@ def count_vectorize(data: pd.DataFrame, conf):
     return X_train_bow
 
 
+# @delayed
+# def modeling(features):
+#     print(features)
 
 @delayed
-def modeling(features):
-    print(features)
+def make_logreg(conf: dict, X, y):
+    parameters = {"C":np.logspace(-3,3,7), "penalty": ["l2"]}
+    logreg = LogisticRegression(random_state=conf['seed'], max_iter=10000)
+    clf = GridSearchCV(logreg, parameters, scoring='f1')
+    clf.fit(X, y)
+    # report.loc[features_combination, modeling_type] = clf.best_score_
+    # print('logreg', report)
+    return clf.best_score_
+
+
+@delayed
+def show_report_table(report: dict):
+    print(pd.DataFrame(report))
+
+
+@delayed
+def concat_features(features):
+    # print(features)
+    return np.concatenate(features, axis=1)
 
 
 if __name__ == '__main__':
     conf = read_config()
     data = load_dataset(conf)
 
-    features = []
+    features = {}
+    report = {}
     for preprocess_type in conf['preprocess']:
         train = eval(preprocess_type + '(data.copy())')
 
         for vectorization_type in conf['feature_engineering']:
             if vectorization_type == 'bow':
                 bow_features = count_vectorize(train, conf['feature_engineering']['bow'])
-                features.append(bow_features)
+                features['bow'] = bow_features.toarray()
             # features = np.concatenate(features, axis=1)
             # modeling(features)
             # print(vectorization_type)
-        # for
+        for features_combination in conf['features_combination']:
+            # print(features_combination)
+            X = concat_features([features[feature]
+                            for feature
+                            in conf['features_combination'][features_combination]])
+            # print(concatenated_features)
+
+            report_by_clsf_type = []
+            for clsf_type in conf['modeling']:
+                if clsf_type == 'logreg':
+
+                    best_score = make_logreg(conf, X, train.label)
+                    report_by_clsf_type.append(best_score)
+                    # report.loc[features_combination, modeling_type] = 1
+            report[features_combination] = report_by_clsf_type
     # preprocess
     # print(features)
-    total = delayed(modeling)(features)
-    # total.visualize()
+    total = delayed(show_report_table)(report)
+    total.visualize()
     res = total.compute()
-    print(res)
+    # print(res)
     # print(data.head())
